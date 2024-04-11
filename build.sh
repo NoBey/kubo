3f2b78e21b93df7a2a9e1ef390c6a8697bbfa770
NoBey <shengpeiyin@bytedance.com>
2024-04-11 20:11:20 +0800

    init

---
+++ build.sh
@@ -0,0 +1,7 @@
+wget https://dist.ipfs.io/go-ipfs/v0.12.2/go-ipfs_v0.12.2_linux-amd64.tar.gz
+
+tar xvfz go-ipfs_v0.4.18_linux-amd64.tar.gz
+
+cd go-ipfs
+sudo ./install.sh
+ipfs --version
--- test/cli/must.go
+++
@@ -1,8 +0,0 @@
-package cli
-
-func MustVal[V any](val V, err error) V {
-	if err != nil {
-		panic(err)
-	}
-	return val
-}
